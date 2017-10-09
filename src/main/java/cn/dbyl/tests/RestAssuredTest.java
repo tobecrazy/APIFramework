e3a5c90d96e2c7457a9bd7326daf53c9af0a5475
tobecrazy <dbyl@foxmail.com>
2017-10-09 22:56:21 +0800

    add mock API

--- src/main/java/cn/dbyl/tests/RestAssuredTest.java
+++ src/main/java/cn/dbyl/tests/RestAssuredTest.java
@@ -1,6 +1,7 @@
 package main.java.cn.dbyl.tests;
 
 import io.restassured.RestAssured;
+
 import static org.hamcrest.CoreMatchers.equalTo;
 
 import org.testng.annotations.Test;
@@ -8,10 +9,33 @@ import org.testng.annotations.Test;
 public class RestAssuredTest {
 	@Test
 	public void getTest() {
-		RestAssured.baseURI = "http://api.douban.com/v2/book";
+		// http://api.map.baidu.com/telematics/v3/weather?location=北京&output=json&ak=6tYzTvGZSOpYB5Oc2YGGOKt8
+		RestAssured.baseURI = "http://api.map.baidu.com/telematics/v3/weather";
 		RestAssured.port = 80;
-		RestAssured.given().param("q", "java8").when().get("/search").then().body("count", equalTo(2));
-		RestAssured.get("/1220562").then().body("title", equalTo("满月之夜白鲸现"));
+		RestAssured.given().param("location", "beijing").param("output", "json").param("ak", "sXF8krbnX1LXbz0TpzivvOS2")
+				.when().get("/weather").then().body("status", equalTo("success")).log();
+
+	}
+
+	@Test
+	public void getTest1() {
+		RestAssured.baseURI = "http://localhost";
+		RestAssured.port = 8080;
+		RestAssured.given().header("Content-Type", "application/json")
+				.header("User-Agent",
+						"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36")
+				.when().get("/girl").then().statusCode(200).body("girl", equalTo("I'm a girl !")).log();
+
+	}
+
+	@Test
+	public void postTest1() {
+		RestAssured.baseURI = "http://localhost";
+		RestAssured.port = 8080;
+		RestAssured.given().header("Content-Type", "application/json")
+				.header("User-Agent",
+						"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36")
+				.when().post("/boy").then().statusCode(200).body("boy", equalTo("I'm a boy !")).log();
 
 	}
 
