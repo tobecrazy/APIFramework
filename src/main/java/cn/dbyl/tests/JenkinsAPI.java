f7611b91d18d8a8232613f8e9a634b774298f876
tobecrazy <dbyl@foxmail.com>
2017-10-19 22:04:12 +0800

    add get and post test

---
+++ src/main/java/cn/dbyl/tests/JenkinsAPI.java
@@ -0,0 +1,42 @@
+package main.java.cn.dbyl.tests;
+
+import io.restassured.RestAssured;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+
+import org.testng.annotations.Test;
+
+public class JenkinsAPI {
+	@Test
+	public void getTest() {
+		// http://api.map.baidu.com/telematics/v3/weather?location=北京&output=json&ak=6tYzTvGZSOpYB5Oc2YGGOKt8
+		RestAssured.baseURI = "http://api.map.baidu.com/telematics/v3/weather";
+		RestAssured.port = 80;
+		RestAssured.given().param("location", "beijing").param("output", "json").param("ak", "sXF8krbnX1LXbz0TpzivvOS2")
+				.when().get("/weather").then().body("status", equalTo("success")).log();
+
+	}
+
+	@Test
+	public void getTest1() {
+		RestAssured.baseURI = "http://localhost";
+		RestAssured.port = 8080;
+		RestAssured.given().header("Content-Type", "application/json")
+				.header("User-Agent",
+						"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36")
+				.when().get("/girl").then().statusCode(200).body("girl", equalTo("I'm a girl !")).log();
+
+	}
+
+	@Test
+	public void postTest1() {
+		RestAssured.baseURI = "http://localhost";
+		RestAssured.port = 8080;
+		RestAssured.given().header("Content-Type", "application/json")
+				.header("User-Agent",
+						"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36")
+				.when().post("/boy").then().statusCode(200).body("boy", equalTo("I'm a boy !")).log();
+
+	}
+
+}
--- src/main/java/cn/dbyl/tests/RestAssuredTest.java
+++ src/main/java/cn/dbyl/tests/RestAssuredTest.java
@@ -2,40 +2,31 @@ package main.java.cn.dbyl.tests;
 
 import io.restassured.RestAssured;
 
+import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.hasItems;
 
 import org.testng.annotations.Test;
 
 public class RestAssuredTest {
-	@Test
-	public void getTest() {
-		// http://api.map.baidu.com/telematics/v3/weather?location=北京&output=json&ak=6tYzTvGZSOpYB5Oc2YGGOKt8
-		RestAssured.baseURI = "http://api.map.baidu.com/telematics/v3/weather";
-		RestAssured.port = 80;
-		RestAssured.given().param("location", "beijing").param("output", "json").param("ak", "sXF8krbnX1LXbz0TpzivvOS2")
-				.when().get("/weather").then().body("status", equalTo("success")).log();
-
-	}
 
 	@Test
 	public void getTest1() {
 		RestAssured.baseURI = "http://localhost";
-		RestAssured.port = 8080;
-		RestAssured.given().header("Content-Type", "application/json")
-				.header("User-Agent",
-						"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36")
-				.when().get("/girl").then().statusCode(200).body("girl", equalTo("I'm a girl !")).log();
+		RestAssured.port = 8081;
+		RestAssured.given().header("Content-Type", "application/json").auth().basic("admin", "3edc4rfv")
+				.header("Authorization", "Basic YWRtaW46M2VkYzRyZnY=").when().get("/jenkins/api/json?pretty=true")
+				.then().statusCode(200).body("jobs.name", hasItems("Demo")).log();
 
 	}
 
 	@Test
-	public void postTest1() {
+	public void postTest() {
 		RestAssured.baseURI = "http://localhost";
-		RestAssured.port = 8080;
-		RestAssured.given().header("Content-Type", "application/json")
-				.header("User-Agent",
-						"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36")
-				.when().post("/boy").then().statusCode(200).body("boy", equalTo("I'm a boy !")).log();
+		RestAssured.port = 8081;
+		RestAssured.given().header("Content-Type", "application/json").auth().basic("admin", "3edc4rfv")
+				.param("test", "Build From API").header("Authorization", "Basic YWRtaW46M2VkYzRyZnY=").when()
+				.post("/jenkins/job/Demo/buildWithParameters").then().statusCode(201).log();
 
 	}
 
