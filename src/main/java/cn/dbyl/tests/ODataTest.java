c968490b2fc415e2d01e4889e6aeb33b43487bb0
I321533 <young.liu@sap.com>
2017-10-27 11:42:38 +0800

    enhance API

--- src/main/java/cn/dbyl/tests/ODataTest.java
+++ src/main/java/cn/dbyl/tests/ODataTest.java
@@ -1,10 +1,5 @@
 package main.java.cn.dbyl.tests;
 
-import static org.hamcrest.CoreMatchers.hasItems;
-
-import java.util.HashMap;
-import java.util.Map;
-
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -12,24 +7,25 @@ import io.restassured.RestAssured;
 import io.restassured.response.Response;
 
 public class ODataTest {
-	String host = "https://api15.sapsf.cn";
+	String host = "https://sfapiqaautocand.sflab.ondemand.com";
 
 	@Test
 	public void getTest1() {
 		RestAssured.baseURI = host;
 
-		Response result = RestAssured.given().auth().basic("admin@atTFTOAV1101", "welcome").get("/odata/v2/FormHeader");
+		Response result = RestAssured.given().auth().basic("cgrant@atTFTV1201", "pwd").param("format", "json")
+				.get("/odata/v2/FormHeader");
 		Assert.assertEquals(result.getStatusCode(), 200);
 		System.out.println(result.getBody().asString());
 		System.out.println(result.xmlPath().get("feed.title").toString());
-		
 
 	}
 
 	@Test
 	public void getTest2() {
 		RestAssured.baseURI = host;
-		Response result = RestAssured.given().auth().basic("admin@atTFTOAV1101", "welcome").param("metadata")
+
+		Response result = RestAssured.given().auth().basic("cgrant@atTFTV1201", "pwd").param("metadata")
 				.get("/odata/v2/");
 		Assert.assertEquals(result.getStatusCode(), 200);
 		System.out.println(result.getBody().asString());
