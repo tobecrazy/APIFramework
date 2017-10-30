892b5ad61478beffa03c67c421037403e2e90a63
tobecrazy <dbyl@foxmail.com>
2017-10-30 23:50:41 +0800

    rm test-out

--- src/main/java/cn/dbyl/tests/ODataDemo.java
+++ src/main/java/cn/dbyl/tests/ODataDemo.java
@@ -1,10 +1,5 @@
 package main.java.cn.dbyl.tests;
 
-import static org.hamcrest.CoreMatchers.hasItems;
-
-import java.util.HashMap;
-import java.util.Map;
-
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -14,19 +9,33 @@ import io.restassured.response.Response;
 public class ODataDemo {
 	String host = "https://api15.sapsf.cn";
 
-	@Test
-	public void getTest1() {
+	// https://api15.sapsf.cn/odata/v2/restricted/FormTemplate?$format=json
+	@Test(enabled = true)
+	public void getTest3() {
 		RestAssured.baseURI = host;
 
-		Response result = RestAssured.given().auth().basic("admin@atTFTOAV1101", "welcome").get("/odata/v2/FormHeader");
-		Assert.assertEquals(result.getStatusCode(), 200);
-		System.out.println(result.getBody().asString());
-		System.out.println(result.xmlPath().get("feed.title").toString());
-		
+		Response result = RestAssured.given().auth().basic("admin@atTFTOAV1101", "welcome").contentType("application/json")
+				.param("format", "json").param("formDataId", "1000000L").header("Content-Type", "application/json")
+				.get("odata/v2/signForm");
+		// Assert.assertEquals(result.getStatusCode(), 200);
+		System.out.println("Test1 >>>>>" + result.prettyPeek().asString());
+		System.out.println("Test1 <<<<<<<" + result.xmlPath().get("feed.title").toString());
 
 	}
 
-	@Test
+//	@Test
+	public void getTest1() {
+		RestAssured.baseURI = host;
+
+		Response result = RestAssured.given().auth().basic("admin@atTFTOAV1101", "welcome")
+				.contentType("application/json").param("format", "json").get("/odata/v2/FormTemplate");
+		Assert.assertEquals(result.getStatusCode(), 200);
+		System.out.println(">>>>>\n\n" + result.prettyPeek().body().prettyPrint());
+		// System.out.println(result.jsonPath().toString());
+
+	}
+
+	// @Test
 	public void getTest2() {
 		RestAssured.baseURI = host;
 		Response result = RestAssured.given().auth().basic("admin@atTFTOAV1101", "welcome").param("metadata")
